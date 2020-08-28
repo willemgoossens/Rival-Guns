8793fd9783dd75106abc5c4953606973f8035cf0
Willem Goossens <goossens.willem@hotmail.com>
2020-08-28 11:09:28 +0200

    Layout changes

--- app/views/locations/bank.php
+++ app/views/locations/bank.php
@@ -14,30 +14,34 @@
         You'll have to look for other ways to stash the green paper.
     </p>
 
-    <div class="row mb-2">
-        <div class="col">
-            <strong>Cash:</strong> &euro;<?php echo $data['user']->cash; ?>
-        </div>
-        <div class="col">
-            <strong>Bank:</strong> &euro;<?php echo $data['user']->bank; ?>
-        </div>
-        <div class="col">
-            <strong>Deposited today:</strong> &euro;<?php echo $data['user']->depositedToday . '/' . GAME_MAX_DEPOSIT; ?>
+    <div class="card">
+        <div class="card-body">
+            <div class="row mb-3">
+                <div class="col">
+                    <strong>Cash:</strong> &euro;<?php echo $data['user']->cash; ?>
+                </div>
+                <div class="col">
+                    <strong>Bank:</strong> &euro;<?php echo $data['user']->bank; ?>
+                </div>
+                <div class="col">
+                    <strong>Deposited today:</strong> &euro;<?php echo $data['user']->depositedToday . '/' . GAME_MAX_DEPOSIT; ?>
+                </div>
+            </div>
+
+            <form action="<?php echo URLROOT; ?>/locations/bank" method="post">
+                <label class="sr-only" for="amount">Amount</label>
+                <div class="input-group mr-sm-2 mb-2">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">&euro;</div>
+                    </div>
+                    <input type="text" id="amount" name="amount" class="form-control <?php echo getValidationClass($data['amountError']); ?>" placeholder="Amount" value="<?php echo $data['amount'] ?? ''; ?>">
+                    <span class="invalid-feedback"><?php echo $data['amountError']; ?></span>
+                </div>
+                <div class="form-row mb-2">
+                    <button type="submit" name="deposit" class="btn btn-primary mr-sm-2">Deposit</button>
+                    <button type="submit" name="withdraw" class="btn btn-secondary mr-sm-2">Withdraw</button>
+                </div>
+            </form>        
         </div>
     </div>
-
-    <form action="<?php echo URLROOT; ?>/locations/bank" method="post">
-        <label class="sr-only" for="amount">Amount</label>
-        <div class="input-group mr-sm-2 mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text">&euro;</div>
-            </div>
-            <input type="text" id="amount" name="amount" class="form-control <?php echo getValidationClass($data['amountError']); ?>" placeholder="Amount" value="<?php echo $data['amount'] ?? ''; ?>">
-            <span class="invalid-feedback"><?php echo $data['amountError']; ?></span>
-        </div>
-        <div class="form-row mb-2">
-            <button type="submit" name="deposit" class="btn btn-primary mr-sm-2">Deposit</button>
-            <button type="submit" name="withdraw" class="btn btn-secondary mr-sm-2">Withdraw</button>
-        </div>
-    </form>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
--- app/views/locations/hospital.php
+++ app/views/locations/hospital.php
@@ -14,20 +14,25 @@
     <p>
         Recently, we've also started our "Poor people program". This means that as long as you are homeless, you can have a rest here.
     </p>
-    <p class="font-italic">
-        As long as you don't own any properties, you can rest here for 5 minutes. During this time, you won't be able to play, but you will gain 10 health and energy points.
-    </p>
 
-    <div class="row mb-2">
-        <div class="col">
-            <strong>Health:</strong><?php echo $data['user']->health; ?>
-        </div>
-        <div class="col">
-            <strong>Energy:</strong><?php echo $data['user']->energy; ?>
+    <div class="card">
+        <div class="card-body">
+            <p class="font-italic">
+                As long as you don't own any properties, you can rest here for 5 minutes. During this time, you won't be able to play, but you will gain 10 health and energy points.
+            </p>
+
+            <div class="row mb-2">
+                <div class="col">
+                    <strong>Health:</strong><?php echo $data['user']->health; ?>
+                </div>
+                <div class="col">
+                    <strong>Energy:</strong><?php echo $data['user']->energy; ?>
+                </div>
+            </div>
+
+            <form action="<?php echo URLROOT; ?>/locations/hospital" method="post">
+                <button type="submit" name="rest" class="btn btn-primary mr-sm-2" <?php echo $data['userHasProperties'] ? "disabled" : ""; ?>>Rest for 5 minutes</button>
+            </form>        
         </div>
     </div>
-
-    <form action="<?php echo URLROOT; ?>/locations/hospital" method="post">
-        <button type="submit" name="rest" class="btn btn-primary mr-sm-2" <?php echo $data['userHasProperties'] ? "disabled" : ""; ?>>Rest for 5 minutes</button>
-    </form>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
