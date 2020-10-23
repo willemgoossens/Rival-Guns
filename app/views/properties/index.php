c0da7e96421df04017c51e1db787d9828dbd4b14
Willem Goossens <goossens.willem@hotmail.com>
2020-10-23 21:45:20 +0200

    Added description of the laundering

--- app/views/properties/index.php
+++ app/views/properties/index.php
@@ -9,6 +9,18 @@
 
     <div class="card mb-3">
         <div class="card-body">
+            <div class="card-text mb-3">
+                <p>
+                    On this page you can launder your cash money to your bankaccount. The more legal companies you have, the more you can launder.
+                    Be careful though, the more you launder, the higher the chance you get arrested. 
+                    The tax services can arrest you for laundering until 2 weeks after the facts!
+                    The maximum laundering amount mentioned below is an estimation of how much can launder, but don't take it for granted! You might get caught for a lower amount.               
+                </p>
+                <p>
+                    When you get caught for laundering, you will be send to jail and receive a fine. If you can't pay the fine the police my confiscate some of your properties.
+                    If one of those properties happens to contain an illegal business, you're screwed!
+                </p>
+            </div>
             <div class="row mb-3">
                 <div class="col">
                     <strong>Cash:</strong> &euro;<?php echo floor( $data['user']->cash ); ?>
